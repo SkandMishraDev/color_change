f1eda6ebb672e2486edf478ff4a076f8b1d3f07d
Skand Mishra <skandmishra9169@gmail.com>
2024-10-07 19:56:17 +0530

    button color change
    color change  file

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState('white')
+
+  return (
+    <>
+      <div className='w-full h-screen duraton-200' style =
+      {{backgroundColor: color}}>
+        <div className ='flex flex-wrap justify-center buttom-12
+            inset-x-0 px-2'>
+        <div className ='flex flex-wrap justify-center gap-3
+            shadow-lg bg-white px-3 py-2 rounded-3xl'>
+              <button 
+              onClick={()=>setColor('red')}
+              className='outline-none px-4 py-1 rounded-full shadow-lg text-black' style={{backgroundcolor:'bg--500'}}>
+                Red
+              </button>
+              <button 
+              onClick={()=>setColor('yellow')}
+              className='outline-none px-4 py-1 rounded-full shadow-lg
+               text-black' style={{backgroundcolor:'yellow'}} >
+                Yellow
+              </button>
+              <button 
+              onClick={()=>setColor('white')}
+              className='outline-none px-4 py-1 rounded-full shadow-lg text-black' style={{backgroundcolor:'bg-white-500'}}>
+                White
+              </button>
+              </div>
+          </div>
+          </div>
+    </>
+  )
+}
+
+export default App
